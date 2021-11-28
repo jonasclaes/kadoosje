5228c667623c1960bcdb026929d6684dc192840e
Jonas Claes <jonas@jonasclaes.be>
2021-11-29 00:18:07 +0100

    Vite with vue-ts

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
-<script setup>
+<script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <h1>Kadoosje</h1>
-  </div>
+  <router-view />
 </template>
