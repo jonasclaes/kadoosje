68dfe6c2688b593b065815a5d89801b9f14481fe
Jonas Claes <jonas@jonasclaes.be>
2021-11-29 15:16:20 +0100

    Env vars

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,12 @@ import {getAuth, onAuthStateChanged} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_APP_API_KEY,
-    authDomain: process.env.FIREBASE_APP_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_APP_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_APP_ID,
+    apiKey: process.env['FIREBASE_APP_API_KEY'],
+    authDomain: process.env['FIREBASE_APP_AUTH_DOMAIN'],
+    projectId: process.env['FIREBASE_APP_PROJECT_ID'],
+    storageBucket: process.env['FIREBASE_APP_STORAGE_BUCKET'],
+    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+    appId: process.env['FIREBASE_APP_APP_ID'],
 };
 
 // Initialize Firebase
