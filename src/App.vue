7951537ab1938ccc87d1e3f25f319143da55ac91
Jonas Claes <jonas@jonasclaes.be>
2021-12-12 20:35:53 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="min-h-screen h-full w-full flex flex-col">
+    <div class="bg-blue-600 text-white">
+      <div class="container mx-auto">
+        <div class="max-w-6xl mx-auto flex justify-between p-4">
+          <div>
+            <img src="./assets/kadoosje.svg" alt="Logo" />
+            <span>Kadoosje</span>
+          </div>
+          <div class="flex space-x-3">
+            <router-link
+              v-for="link in navbarLinks"
+              :key="link.name"
+              :to="link.route"
+              :class="{ 'font-semibold': $route.name === link.name }"
+              >{{ link.name }}</router-link
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="bg-gray-100 text-gray-800 min-h-screen h-full w-full absolute top-0 pt-14"
+    >
+      <div class="container mx-auto">
+        <div class="max-w-6xl mx-auto p-4">
+          <!-- <router-view /> -->
+        </div>
+      </div>
+    </div>
   </div>
-  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { Vue } from "vue-class-component";
 
-#nav {
-  padding: 30px;
+export default class App extends Vue {
+  navbarLinks = [
+    {
+      name: "Home",
+      route: "/",
+    },
+  ];
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
